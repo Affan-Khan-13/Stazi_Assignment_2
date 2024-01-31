aafe17f2956db9dff181c72f2cdfaca73ff1c18a
Affan-Khan-13 <indushassan@gmail.com>
2024-01-31 10:29:04 +0530

    pushing

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
 import { Link } from 'react-router-dom';
 import './App.css';
 import Header from './components/header';
@@ -6,6 +8,9 @@ import Cards from './components/cards';
 function App() {
   return (
     <>
+      <Helmet>
+        <script id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="a3f0407d-03f0-4934-ab4e-0b26f137a1cd" data-blockingmode="auto" type="text/javascript"></script>
+      </Helmet>
       <div className='px-20 bg-[#eeecec]'>
         <Header />
         <Cards />
