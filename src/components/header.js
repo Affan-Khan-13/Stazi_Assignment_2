3315664b6db79001609a66dabcfdaead7103476b
Affan-Khan-13 <indushassan@gmail.com>
2024-01-31 10:19:09 +0530

    old

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className='flex flex-col items-center py-12'>
       <div className='font-bold text-[2rem] text-[#3f4042]'>
-        Featured Listed Propert
+        Featured Listed Property
       </div>
       <div className=' w-[30rem] font-medium text-[#3f4042]'>
         Real Estate can be bought, sold, leased and can be a valuable investment oppurtunity. The value of real estate can be .....
